295dca4752abf38ddd89db62fc86a34688425184
wanedev <wanesmith.dev@gmail.com>
2022-06-30 19:23:09 -0700

    first commit

---
+++ questionaire.sh
@@ -0,0 +1,8 @@
+echo "Who is M-creepa?"
+read name
+
+if [ $name == "smidget" ]; then
+echo "$name is one of the greatest web-developers alive."
+else
+echo "Sorry, that name doesn't match the correct name."
+fi
