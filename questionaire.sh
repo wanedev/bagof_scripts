008851bb946798d46457c7f76d2ee68373a10c51
wanedev <wanesmith.dev@yahoo.com>
2022-07-02 17:17:15 -0700

    first commit

--- questionaire.sh
+++ questionaire.sh
@@ -1,8 +1,15 @@
-echo "Who is M-creepa?"
+echo "Name 1 out of 2 TRI*NC developers"
 read name
 
 if [ $name == "smidget" ]; then
-echo "$name is one of the greatest web-developers alive."
+echo "You betta know it, $name is one of the greatest web-developers alive..!"
+elif [ $name == "mskeezy" ]; then
+echo "That will also work my man."
+elif [ $name == "ton" ]; then
+echo"You know $name had something to do with this $script..!"
+elif [ $name == "wanescript" ]; then
+echo "Off top, he definitely had something to do with this $script frfr..!"
+
 else
-echo "Sorry, that name doesn't match the correct name."
+echo "Nope don't recognize $name."
 fi
