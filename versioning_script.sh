8c2f83146de7a56fae2a7fab934dfbc8d9a0832f
wanedev <wanesmith.dev@yahoo.com>
2022-07-18 19:46:07 -0700

    first initial commit

---
+++ versioning_script.sh
@@ -0,0 +1,20 @@
+#!bin\bash
+
+echo "This script was created and developed by:"
+sleep 4
+cat wanescript2.txt
+echo
+
+echo "This script will display the version info about nodejs, npm, and git."
+sleep 5
+echo "Your version of nodejs is:"
+node --version
+sleep 2
+echo "Your version of npm is:"
+npm --version
+sleep 3
+echo "Your version of git is:"
+git --version
+echo "Script finnished...x"
+sleep 2
+echo "This concludes the end of this versioning script."
