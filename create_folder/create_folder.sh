f84f855ffcb64656778fc742680b4f098a14c242
wanedev <wanesmith.dev@yahoo.com>
2022-07-21 19:26:13 -0700

    file changes

--- create_folder/create_folder.sh
+++ create_folder/create_folder.sh
@@ -5,7 +5,7 @@ mkdir script-bag
 cd script-bag
 # The above steps are only if you want to make your own folder on your desktop, then create a script inside it for organization purposes.
 
-echo "Create a script name for your .sh file."
+echo "Create a script name for your .sh and .txt file."
 read name
 touch $name.sh
 echo "$name.sh and $name.txt have been created. cd into script-bag to interact with your newly created files."
