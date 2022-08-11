8ba7bf5aba9170b2a92c883d6b268fd841baafc1
wanedev <wanesmith.dev@yahoo.com>
2022-08-10 19:11:06 -0700

    file changes

--- print-string/print-string.sh
+++ print-string/print-string.sh
@@ -3,9 +3,11 @@
 
 # The below line will simply print a string to your terminal.
 
-echo "The echo command will print a string in the terminal"
+echo "The echo command will print a string in the terminal."
 
 
 
-#below for cmd to run your file.
+# below are two different commands to run your file.
+
 # bash print-string.sh
+# ./print-string.sh
